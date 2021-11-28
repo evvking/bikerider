f564e09e73cba0bda176ac0470efb37c8bd2c3bc
Evvking <87782409+evvking@users.noreply.github.com>
2021-11-28 18:01:18 +0100

    Fix errors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var n_name = 'Podjazd';
 var distance = 0;
 var startAltitude = 0;
 var endAltitude = 0;
@@ -22,6 +23,7 @@ document.getElementById('b_clean').addEventListener('click', function() {
 });
 
 document.getElementById('b_move').addEventListener('click', function() {
+    n_name = document.getElementById('p_name').value;
     let pName = document.getElementById('p_name').value;
     document.getElementById('sum_check').textContent = pName;
     distance = parseFloat(document.getElementById('p_distance').value);
@@ -119,9 +121,180 @@ document.getElementById('b_move').addEventListener('click', function() {
     document.getElementById('sum_wind').textContent = 'Wiatr: ' + pWind;
 });
 
+/**
+ * Ta funkcja wyświetla stopień trudnosci podjazdu
+ * 
+ * Kategorie:
+ * A1: 0 - 199p
+ * A1+: 200 - 399p
+ * A2: 400 - 599p
+ * A2+: 600 - 799p
+ * B1: 800 - 999p
+ * B1+: 1000 - 1199p
+ * B2: 1200 - 1399p
+ * B2+: 1400 - 1599p
+ * C1: 1600 - 1799p
+ * C1+: 1800 - 1999p
+ * C2: 2000 - 2199p
+ * C2+: 2200 - 2399p
+ * D1: 2400 - 2599p
+ * D1+: 2600 - 2799p
+ * D2: 2800 - 2999p
+ * E: 3000p+
+ */
 document.getElementById('b_check').addEventListener('click', function() {
     var difficultyPoints = Math.round(difficulty());
     document.getElementById('diff-res').textContent = difficultyPoints + ' punktów';
+    var category = '0';
+
+    if (difficultyPoints < 200) {
+        category = 'A1';
+    } else if (difficultyPoints < 400) {
+        category = 'A1+';
+    } else if (difficultyPoints < 600) {
+        category = 'A2';
+    } else if (difficultyPoints < 800) {
+        category = 'A2+';
+    } else if (difficultyPoints < 1000) {
+        category = 'B1';
+    } else if (difficultyPoints < 1200) {
+        category = 'B1+';
+    } else if (difficultyPoints < 1400) {
+        category = 'B2';
+    } else if (difficultyPoints < 1600) {
+        category = 'B2+';
+    } else if (difficultyPoints < 1800) {
+        category = 'C1';
+    } else if (difficultyPoints < 2000) {
+        category = 'C1+';
+    } else if (difficultyPoints < 2200) {
+        category = 'C2';
+    } else if (difficultyPoints < 2400) {
+        category = 'C2+';
+    } else if (difficultyPoints < 2600) {
+        category = 'D1';
+    } else if (difficultyPoints < 2800) {
+        category = 'D1+';
+    } else if (difficultyPoints < 3000) {
+        category = 'D2';
+    } else {
+        category = 'E';
+    }
+
+    switch (category) {
+        case 'A1':
+            document.getElementById('diff-cat').textContent = 'Kategoria: A1';
+            document.getElementById('diff-des').textContent = 'Bardzo łatwy';
+            let descriptionA1 = 'Podjazd bardzo łatwy. Nie wymaga żadnego przygotowania przed wjazdem. Odpowiedni dla wszystkich poziomów zaawansowania.';
+            document.getElementById('diff-mes').textContent = descriptionA1;
+            document.getElementById('div-result').style.backgroundColor = '#2091c9';
+            break;
+        case 'A1+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: A1+';
+            document.getElementById('diff-des').textContent = 'Bardzo łatwy (trudniejszy)';
+            let descriptionA1s = 'Podjazd bardzo łatwy, ale nieco trudniejszy. Zalecana jest lekka rozgrzewka przed wjazdem. Odpowiedni dla wszystkich poziomów zaawansowania.';
+            document.getElementById('diff-mes').textContent = descriptionA1s;
+            document.getElementById('div-result').style.backgroundColor = '#20c996';
+            break;
+        case 'A2':
+            document.getElementById('diff-cat').textContent = 'Kategoria: A2';
+            document.getElementById('diff-des').textContent = 'Łatwy';
+            let descriptionA2 = 'Podjazd łatwy. Zalecana jest rozgrzewka przed wjazdem. Zalecane minimalne doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionA2;
+            document.getElementById('div-result').style.backgroundColor = '#20c93f';
+            break;
+        case 'A2+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: A2+';
+            document.getElementById('diff-des').textContent = 'Łatwy (trudniejszy)';
+            let descriptionA2s = 'Podjazd łatwy, ale nieco trudniejszy. Zalecana jest rozgrzewka przed wjazdem. Zalecane minimalnie większe doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionA2s;
+            document.getElementById('div-result').style.backgroundColor = '#88c920';
+            break;
+        case 'B1':
+            document.getElementById('diff-cat').textContent = 'Kategoria: B1';
+            document.getElementById('diff-des').textContent = 'Średnio łatwy';
+            let descriptionB1 = 'Podjazd o zwiększonym stopniu trudności. Zalecana jest solidna rozgrzewka przed wjazdem. Zalecane co najmniej 2-letnie doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionB1;
+            document.getElementById('div-result').style.backgroundColor = '#d9ff00';
+            break;
+        case 'B1+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: B1+';
+            document.getElementById('diff-des').textContent = 'Średnio łatwy (trudniejszy)';
+            let descriptionB1s = 'Podjazd o zwiększonym stopniu trudności, dodatkowo utrudniony. Zalecana jest solidna rozgrzewka przed wjazdem. Zalecane co najmniej dobre doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionB1s;
+            document.getElementById('div-result').style.backgroundColor = '#c4d61c';
+            break;
+        case 'B2':
+            document.getElementById('diff-cat').textContent = 'Kategoria: B2';
+            document.getElementById('diff-des').textContent = 'Średnio trudny';
+            let descriptionB2 = 'Podjazd o bardzo zwiększonym stopniu trudności. Zalecana jest bardzo solidna rozgrzewka przed wjazdem. Zalecane co najmniej dobre doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionB2;
+            document.getElementById('div-result').style.backgroundColor = '#d6ba1c';
+            break;
+        case 'B2+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: B2+';
+            document.getElementById('diff-des').textContent = 'Średnio trudny (trudniejszy)';
+            let descriptionB2s = 'Podjazd o bardzo zwiększonym stopniu trudności, dodatkowo utrudniony. Zalecana jest bardzo solidna rozgrzewka przed wjazdem. Zalecane co najmniej bardzo dobre doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionB2s;
+            document.getElementById('div-result').style.backgroundColor = '#d69e1c';
+            break;
+        case 'C1':
+            document.getElementById('diff-cat').textContent = 'Kategoria: C1';
+            document.getElementById('diff-des').textContent = 'Trudny';
+            let descriptionC1 = 'Podjazd trudny. Zalecane jest zaliczenie kilku średnio trudnych podjazdów przed wjazdem. Zalecane co najmniej bardzo dobre doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionC1;
+            document.getElementById('div-result').style.backgroundColor = '#ff5500';
+            break;
+        case 'C1+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: C1+';
+            document.getElementById('diff-des').textContent = 'Trudny (zaawansowany)';
+            let descriptionC1s = 'Podjazd trudny, z dodatkowymi trudnościami. Zalecane jest zaliczenie kilku średnio trudnych podjazdów o poziomie co najmniej B1+ przed wjazdem. Zalecane co najmniej 5-letnie doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionC1s;
+            document.getElementById('div-result').style.backgroundColor = '#ff0000';
+            break;
+        case 'C2':
+            document.getElementById('diff-cat').textContent = 'Kategoria: C2';
+            document.getElementById('diff-des').textContent = 'Bardzo trudny';
+            let descriptionC2 = 'Podjazd bardzo trudny. Zalecane jest zaliczenie kilku trudnych podjazdów o poziomie co najmniej C1 przed wjazdem. Zalecane co najmniej 5-letnie doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionC2;
+            document.getElementById('div-result').style.backgroundColor = '#ba0000';
+            break;
+        case 'C2+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: C2+';
+            document.getElementById('diff-des').textContent = 'Bardzo trudny (zaawansowany)';
+            let descriptionC2s = 'Podjazd bardzo trudny, z dodatkowymi trudnościami. Zalecane jest zaliczenie kilku trudnych podjazdów o poziomie co najmniej C1+ przed wjazdem. Zalecane co najmniej 8-letnie doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionC2s;
+            document.getElementById('div-result').style.backgroundColor = '#920000';
+            break;
+        case 'D1':
+            document.getElementById('diff-cat').textContent = 'Kategoria: D1';
+            document.getElementById('diff-des').textContent = 'Ekstremalnie trudny';
+            let descriptionD1 = 'Podjazd ekstremalnie trudny. Zalecane jest zaliczenie kilku bardzo trudnych podjazdów o poziomie co najmniej C2 przed wjazdem. Zalecane co najmniej 8-letnie doświadczenie.';
+            document.getElementById('diff-mes').textContent = descriptionD1;
+            document.getElementById('div-result').style.backgroundColor = '#750000';
+            break;
+        case 'D1+':
+            document.getElementById('diff-cat').textContent = 'Kategoria: D1+';
+            document.getElementById('diff-des').textContent = 'Ekstremalnie trudny (zaawansowany)';
+            let descriptionD1s = 'Podjazd ekstremalnie trudny, z dodatkowymi trudnościami. Zalecane jest zaliczenie kilku bardzo trudnych podjazdów o poziomie co najmniej C2+ przed wjazdem. Zalecane co najmniej 10-letnie doświadczenie. Oprócz tego, wymagana jest perfekcyjna kondycja aerobowa i anaerobowa, a także niesamowicie wydajne serce.';
+            document.getElementById('diff-mes').textContent = descriptionD1s;
+            document.getElementById('div-result').style.backgroundColor = '#570000';
+            break;
+        case 'D2':
+            document.getElementById('diff-cat').textContent = 'Kategoria: D2';
+            document.getElementById('diff-des').textContent = 'Ekstremalny';
+            let descriptionD2 = 'Podjazd ekstremalny. Wjazd zalecany wyłącznie dla zawodowców. Zalecane jest zaliczenie kilku ekstremalnie trudnych podjazdów o poziomie co najmniej D1 przed wjazdem. Zalecane co najmniej 20-letnie doświadczenie. Oprócz tego, wymagana jest niezwykle perfekcyjna kondycja aerobowa i anaerobowa, a także perfekcyjnie wydajne serce, przygotowane na gigantyczny wysiłek.';
+            document.getElementById('diff-mes').textContent = descriptionD2;
+            document.getElementById('div-result').style.backgroundColor = '#570000';
+            break;
+        case 'E':
+            document.getElementById('diff-cat').textContent = 'Kategoria: E';
+            document.getElementById('diff-des').textContent = 'Zabójczy';
+            let descriptionE = 'Podjazd zabójczy. Wjeżdzając na podjazd ryzykuje się życiem. Zalecane zastanowienie się, czy na pewno warto. Niezbędne ogromne doświadczenie i wytrzymałość. Oprócz tego, wymagana jest niezwykle perfekcyjna kondycja aerobowa i anaerobowa, a także perfekcyjnie wydajne serce, przygotowane na gigantyczny wysiłek, który może okazać się zabójczy!';
+            document.getElementById('diff-mes').textContent = descriptionE;
+            document.getElementById('div-result').style.backgroundColor = '#000000';
+            break;
+    }
 });
 
 /**
